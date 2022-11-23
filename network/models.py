b36c67c93800464f150a7693fda25569e8e51d5d
ca-Stella <stella.2532@gmail.com>
2022-11-23 15:11:28 -0700

    Add new post form

--- network/models.py
+++ network/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name ="posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
--- network/views.py
+++ network/views.py
@@ -4,8 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
-
+from .models import User, Post
 
 def index(request):
     return render(request, "network/index.html")
