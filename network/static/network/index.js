990a5fb2a5566e13178a1c1c796d0ae08de5c67a
ca-Stella <stella.2532@gmail.com>
2022-11-29 17:26:44 -0700

    Add textarea when editing

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
+    const editBtn = document.querySelector('.edit-btn');
+    editBtn.addEventListener('click', editPost);
 })
+
+function editPost() {
+    const editBox = document.createElement('textarea');
+    const editBtn = document.querySelector('.edit-btn');
+    const parentDiv = editBtn.parentNode;
+
+    parentDiv.append(editBox);
+}
