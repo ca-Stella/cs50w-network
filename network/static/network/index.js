be944dbc5a08c7031bc055ff6db73c58e2e1834b
ca-Stella <stella.2532@gmail.com>
2022-12-01 19:09:20 -0700

    Clean and better style edit function

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function editPost() {
     const postid = this.classList[1];
-    console.log(postid);
+    const postContent = document.querySelector(`#post-content-${postid}`);
     const editblock = document.querySelector(`#edit-block-${postid}`);
-    editblock.style.display = 'block';
     const editBox = document.querySelector(`#edit-${postid}`);
-    editBox.innerHTML = document.querySelector(`#post-content-${postid}`).innerHTML;
+
+    editblock.style.display = 'block';
+    postContent.style.display = 'none';
+    editBox.innerHTML = postContent.innerHTML;
 
     const saveBtn = document.querySelector(`#save-${postid}`);
     saveBtn.addEventListener('click', updatePost);
@@ -21,6 +23,8 @@ function editPost() {
 function updatePost() {
     const postid = this.classList[1];
     const editContent = document.querySelector(`#edit-${postid}`).value;
+    const postContent = document.querySelector(`#post-content-${postid}`);
+
     fetch(`/edit/${postid}`, {
         method: 'PUT',
         body: JSON.stringify({
@@ -30,6 +34,7 @@ function updatePost() {
     .then(result => {
         const editblock = document.querySelector(`#edit-block-${postid}`);
         editblock.style.display = 'none';
+        postContent.style.display = 'block';
         document.querySelector(`#post-content-${postid}`).innerHTML = editContent;
     })
 }
